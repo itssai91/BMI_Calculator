8796a03bcece36a8095ee70314ac1ba93b85233b
itssai91 <44494014+itssai91@users.noreply.github.com>
2020-04-06 21:55:41 +0530

    Initial commit

---
+++ custom.js
@@ -0,0 +1,44 @@
+//Subscribe my Youtube Channel name "TRACKNTECH"
+
+function bmi() {
+    //Get feet and inches Value
+    let feet = document.querySelector(".feet").value;
+    let inch = document.querySelector(".inch").value;
+    // Merge feet and inches Value in one
+    let metre = feet + "." + inch;
+    //feet to metre conversion and get The height in metre
+    let height = metre / 3.2808;
+    // Get Weight Value
+    let weight = document.querySelector(".weight").value;
+    //BMI Calculation
+    let bmi = weight / Math.pow(height, 2);
+    //BMI Showing in P Tag With condition
+    if (bmi < 18.5) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Under Weight";
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Over Weight";
+    } else if (bmi > 30) {
+        document.querySelector("p").innerHTML =
+            "Your Body Mass Index (BMI) is-: " +
+            (Math.round(bmi * 100) / 100).toFixed(2) +
+            "<br>" +
+            " Index Value is in Obesity";
+    }
+}
+//Click Event on Calculate Button
+document.querySelector(".btn").addEventListener("click", bmi);
+2 Attachments
